b1ba8dadafd0fdf6f19923c7554047d4c778b65c
Sen Khvorov <khvorov45@gmail.com>
2019-07-22 10:30:42 +1000

    Added Compiler class

---
+++ jackcompiler/compiler.py
@@ -0,0 +1,10 @@
+"""Top-level compiler control"""
+
+class JackCompiler():
+    """Top-level control class"""
+    def __init__(self):
+        pass
+
+    def run(self, system_arguments):
+        """Runs the compiler"""
+        pass
